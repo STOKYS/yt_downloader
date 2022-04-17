c4b20021799ae0c7c7404742a2ceeb5e4e662e9a
David Stoček <stocekdavid.net@gmail.com>
2022-04-17 12:37:07 +0200

    Update v22.04.17:13 - [Tried GUI]
    Probably won't work

--- main.py
+++ main.py
@@ -4,15 +4,41 @@ import re
 import subprocess
 from pytube import YouTube
 from pytube import Playlist
+import tkinter as tk
 
 
-def main():
-    download_pytube()
+def gui():
+    # init tkinter
+    root = tk.Tk()
+    root.title("GUI")
+    root.geometry("800x600")
+
+    # init label
+    label = tk.Label(root, text="Hello World!")
+    label.grid()
+
+    # init button
+    button = tk.Button(root, text="Click Me!", command=lambda: download_pytube(str(entry.get())))
+    button.grid(column=1, row=1)
+
+    # init entry
+    entry = tk.Entry(root, width=100)
+    entry.grid(column=0, row=1)
+
+    # init listbox
+    listbox = tk.Listbox(root)
+    listbox.grid()
+    # listbox.insert(tk.END, "Hello")
+    # listbox.insert(tk.END, "World")
+    # listbox.insert(tk.END, "!")
+
+    # init tkinter
+    root.mainloop()
 
 
-def download_pytube():
+def download_pytube(url):
     home = expanduser("~/Music")
-    url = input("Paste URL of the audio you want to download: ")
+    # url = input("Paste URL of the audio you want to download: ")
     if url.find('list=') != -1:
         print("Downloading playlist...")
         download_playlist(url, home)
@@ -54,6 +80,11 @@ def subprocess_run(home, x):
         os.path.join(home, f"{x.title}.mp3")
     ], stdout=subprocess.DEVNULL)
     subprocess.run(['rm', f'{home}/{x.title}.mp4'], stdout=subprocess.DEVNULL)
+
+
+def main():
+    gui()
+    download_pytube()
 
 
 if __name__ == '__main__':
